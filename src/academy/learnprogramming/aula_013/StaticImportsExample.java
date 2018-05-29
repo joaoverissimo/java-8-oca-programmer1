f05f7527caadd1d1d5d0058567c23e8ef7e4b671
joaoverissimo <joao-verissimo@hotmail.com>
2018-05-28 23:13:00 -0300

    add arquivos das aulas: aula_013;

---
+++ src/academy/learnprogramming/aula_013/StaticImportsExample.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming.aula_013;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.min;
+import static java.lang.System.out;
+import static academy.learnprogramming.aula_013.Config.*;
+
+public class StaticImportsExample {
+
+    public static void main(String[] args) {
+        int min = min(5,7);
+        out.println("min= " + min);
+        out.println(PI);
+
+        printConfig();
+
+        out.println("name= " + NAME);
+        out.println("columnCount= " + MAX_COLUMN_COUNT);
+    }
+}
