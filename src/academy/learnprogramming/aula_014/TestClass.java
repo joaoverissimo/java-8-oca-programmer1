eb37d8380bb717182c6b15002f7dea30157c0fb1
joaoverissimo <joao-verissimo@hotmail.com>
2018-05-28 23:15:06 -0300

    add arquivos das aulas: aula_014; aula_015; aula_016;

---
+++ src/academy/learnprogramming/aula_014/TestClass.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming.aula_014;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i = 0;
+        long l;
+        float f;
+        double d;
+        System.out.println(i);
+    }
+
+}
