f17d520510df6dfd6aa6b024280f516812ff90b3
joaoverissimo <joao-verissimo@hotmail.com>
2018-06-22 02:00:02 +0000

    add arquivos das aulas: aula_082; aula_083;

---
+++ src/academy/learnprogramming/aula_083/AnotherMain.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming.aula_083;
+
+public class AnotherMain {
+
+    public static void main(String[] args) {
+        System.out.println("print in another main");
+    }
+}
