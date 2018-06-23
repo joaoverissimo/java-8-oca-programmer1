da9fd8694fbb615290a016d9b13b9df60537b7c1
joaoverissimo <joao-verissimo@hotmail.com>
2018-06-23 02:00:02 +0000

    add arquivos das aulas: aula_083; aula_084;

---
+++ src/academy/learnprogramming/aula_083/Launcher.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.aula_083;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        StaticMethodsAndFields.main(args);
+        System.out.println(StaticMethodsAndFields.myNumber);
+
+        AnotherMain.main(args);
+    }
+}
---
+++ src/academy/learnprogramming/aula_083/StaticMethodsAndFields.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming.aula_083;
+
+public class StaticMethodsAndFields {
+
+    public static int myNumber = 10;
+
+    public static void main(String[] args) {
+        System.out.println(myNumber);
+    }
+}
