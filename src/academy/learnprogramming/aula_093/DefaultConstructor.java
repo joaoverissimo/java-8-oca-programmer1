b079af3f7d59d1794a7f7c4339a957dcdcdb53e5
joaoverissimo <joao-verissimo@hotmail.com>
2018-06-29 02:00:01 +0000

    add arquivos das aulas: aula_092; aula_093;

---
+++ src/academy/learnprogramming/aula_093/DefaultConstructor.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming.aula_093;
+
+public class DefaultConstructor {
+
+    public static void main(String[] args) {
+//        Dog dog = new Dog();
+    }
+}
---
+++ src/academy/learnprogramming/aula_093/Dog.java
@@ -0,0 +1,6 @@
+package academy.learnprogramming.aula_093;
+
+public class Dog {
+
+    public Dog() {}
+}
