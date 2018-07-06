1a0b8f680e21c4215e309f03f2fff348e65f24f7
joaoverissimo <joao-verissimo@hotmail.com>
2018-07-06 02:00:01 +0000

    add arquivos das aulas: aula_100;

---
+++ src/academy/learnprogramming/aula_100/Cat.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming.aula_100;
+
+public class Cat extends Animal {
+
+    public Cat(int age) {
+        super(age);
+    }
+}
---
+++ src/academy/learnprogramming/aula_100/Dog.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming.aula_100;
+
+public class Dog extends Animal {
+
+    public Dog(int age) {
+        super(age);
+        System.out.println("Dog");
+
+    }
+}
