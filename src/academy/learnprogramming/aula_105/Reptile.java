52f6983613a634a15de71ce42eee7731d41ffd3b
joaoverissimo <joao-verissimo@hotmail.com>
2018-08-17 02:00:01 +0000

    add arquivos das aulas: aula_105;

---
+++ src/academy/learnprogramming/aula_105/Reptile.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming.aula_105;
+
+public class Reptile {
+
+    protected boolean hasLegs() {
+        return true;
+    }
+
+    double getWeight() {
+        return 2.0;
+    }
+}
