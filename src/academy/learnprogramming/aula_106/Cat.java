d13b8d5db251e57be2c1f7f08b93a9a913fe0c7c
joaoverissimo <joao-verissimo@hotmail.com>
2018-08-18 02:00:01 +0000

    add arquivos das aulas: aula_106;

---
+++ src/academy/learnprogramming/aula_106/Cat.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming.aula_106;
+
+public class Cat extends Animal {
+
+    public Cat(int age) {
+        super(age);
+    }
+
+    @Override
+    public int getWeight() {
+        return 5;
+    }
+
+    @Override
+    public void printName() {
+
+    }
+}
