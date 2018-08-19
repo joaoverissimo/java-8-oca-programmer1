3d2c40bfd1d5b67b88d46728f918ab646e509c47
joaoverissimo <joao-verissimo@hotmail.com>
2018-08-19 02:00:01 +0000

    add arquivos das aulas: aula_106;

---
+++ src/academy/learnprogramming/aula_106/HasTail.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.aula_106;
+
+public interface HasTail {
+
+//    public static final int DEFAULT_TAIL_LENGTH = 2;
+    int DEFAULT_TAIL_LENGTH = 2;
+
+//    public int getTailLength();
+//    public abstract int getTailLength();
+    int getTailLength();
+    
+//    int getWeight() {}
+}
