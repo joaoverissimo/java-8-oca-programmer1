723bfe3d04bf0906fc16b622d0a2f8d81f1f3394
joaoverissimo <joao-verissimo@hotmail.com>
2018-08-21 02:00:01 +0000

    add arquivos das aulas: aula_106;

---
+++ src/academy/learnprogramming/aula_106/Rabbit.java
@@ -0,0 +1,34 @@
+package academy.learnprogramming.aula_106;
+
+public class Rabbit extends Animal {
+
+    public Rabbit() {
+//        super; // does not compile
+//        super().setAge(3);
+
+        super();
+//        super.setAge(3);
+//        this.setAge(3);
+//        setAge(3);
+    }
+
+    public Rabbit(int age) {
+//        this(age); recursive call
+
+//        this();
+//        super();
+//        this();
+//        setAge(3);
+        super(3);
+    }
+
+    @Override
+    public int getWeight() {
+        return 2;
+    }
+
+    @Override
+    public void printName() {
+
+    }
+}
