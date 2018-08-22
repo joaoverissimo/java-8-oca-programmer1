838f0bc494cc3c87d8cd8ee6fa594dba11eeb5e7
joaoverissimo <joao-verissimo@hotmail.com>
2018-08-22 02:00:01 +0000

    add arquivos das aulas: aula_106;

---
+++ src/academy/learnprogramming/aula_106/main/Main.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming.aula_106.main;
+
+import academy.learnprogramming.aula_106.Husky;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Husky husky = new Husky(5);
+        husky.setName("Rex");
+        husky.printDetails();
+        husky.setAge(6);
+//        husky.printName();
+        husky.printDetails();
+        System.out.println("weight= " + husky.getWeight());
+
+//        Dog dog = new Dog(3);
+//        dog.setName("Rex");
+//        dog.printDetails();
+
+        husky.eat();
+        System.out.println("avg= " + husky.getAverageWeight());
+
+
+    }
+}
