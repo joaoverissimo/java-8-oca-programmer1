ff95d9de7d262c2b4ff0bc0c58a01c31f63456af
joaoverissimo <joao-verissimo@hotmail.com>
2018-08-23 02:00:01 +0000

    add arquivos das aulas: aula_108;

---
+++ src/academy/learnprogramming/aula_108/Bear.java
@@ -0,0 +1,24 @@
+package academy.learnprogramming.aula_108;
+
+public class Bear extends Animal implements Omnivore {
+
+    @Override
+    public void eatPlants() {
+        System.out.println("Bear eating plants");
+    }
+
+    @Override
+    public void eatMeat() {
+        System.out.println("Bear eating meat");
+    }
+
+    @Override
+    public int getWeight() {
+        return 2000;
+    }
+
+    @Override
+    protected void printName() {
+        System.out.println("Bear");
+    }
+}
